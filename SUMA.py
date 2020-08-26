9984c6e6473eed41be194a89c99776b526acfaa7
Melanie <171070@unsaac.edu.pe>
2020-08-26 01:20:23 -0500

    segundo commit

--- SUMA.py
+++ SUMA.py
@@ -1,6 +1,8 @@
 def suma(a,b):
     return a+b
-
+def resta(a,b):
+    return a-b
 a=int(input("Ingrese primer numero:"))
 b=int(input("Ingrese segundo numero:"))
 print suma(a,b)
+print resta(a,b) 
